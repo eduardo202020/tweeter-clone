81ac5b8d2268c4fa1f098c613077ea92d8da24dc
Eduardo26 <jguevaral@uni.pe>
2023-04-16 18:18:03 -0500

    chang currentUser for useSession in UserBio

--- components/users/UserBio.tsx
+++ components/users/UserBio.tsx
@@ -14,8 +14,8 @@ interface UserBioProps {
 }
 
 const UserBio: React.FC<UserBioProps> = ({ userId }) => {
-  // const { data: currentUser } = useSession();
-  const { data: currentUser } = useCurrentUser();
+  const { data: currentUser } = useSession();
+  // const { data: currentUser } = useCurrentUser();
   const { data: fetchedUser } = useUser(userId);
 
   const editModal = useEditModal();
@@ -34,7 +34,7 @@ const UserBio: React.FC<UserBioProps> = ({ userId }) => {
   return (
     <div className="border-b-[1px] border-neutral-800 pb-4">
       <div className="flex justify-end p-2">
-        {currentUser?.id === userId ? (
+        {currentUser?.user.id === userId ? (
           <Button secondary label="Edit" onClick={editModal.onOpen} />
         ) : (
           <Button
